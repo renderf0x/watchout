e62fa2630d3604e89c6e859db4a6d79ec45d1a62
renderf0x <derek@barncard.com>
2014-11-07 14:24:39 -0800

    gameboard

--- watchout.js
+++ watchout.js
@@ -1,2 +1,9 @@
 // start slingin' some d3 here.
 
+var gameOptions = {
+  height: 450,
+  width: 700
+}
+
+var svgContainer = d3.select('.gameboard').append('svg')
+  .attr('width', gameOptions.width).attr('height', gameOptions.height);
