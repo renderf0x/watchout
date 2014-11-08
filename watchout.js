8fd303e40e65379d9ae8e8bc89110fe94140b00b
renderf0x <derek@barncard.com>
2014-11-07 17:21:22 -0800

    Make a differently-colored dot to represent the player. Make it draggable.

--- watchout.js
+++ watchout.js
@@ -15,10 +15,6 @@ var scale = {
 var svgContainer = d3.select('.gameboardContainer').append('svg')
   .attr('width', gameOptions.width).attr('height', gameOptions.height);
 
-
-
-d3.select('svg').append('circle').attr('r', 10).attr('cx', 50).attr('cy', 50);
-
 var createEnemies = function(){
   return _.range(0, gameOptions.nEnemies).map(function(index){
     return {
@@ -29,25 +25,25 @@ var createEnemies = function(){
   });
 };
 
-var enemies = createEnemies();
-
-svgContainer.selectAll('circle').data(enemies)
-  .enter()
-  .append('circle')
-  .attr('r', 10)
-  .attr('enemy_id', function(enemy) {
-    return enemy.enemy_id;
-  })
-  .attr('cx', function(enemy) {
-    return scale.x(enemy.x);
-  }).attr('cy', function(enemy) {
-    return scale.y(enemy.y);
-  });
+var renderEnemies = function() {
+  svgContainer.selectAll('circle').data(enemies)
+    .enter()
+    .append('circle')
+    .attr('r', 10)
+    .attr('enemy_id', function(enemy) {
+      return enemy.enemy_id;
+    })
+    .attr('cx', function(enemy) {
+      return scale.x(enemy.x);
+    }).attr('cy', function(enemy) {
+      return scale.y(enemy.y);
+    })
+    .classed('enemy', true);
+}
 
 var moveEnemies = function() {
-  svgContainer.selectAll('circle').transition()
+  svgContainer.selectAll('.enemy').transition()
     .duration(gameOptions.intervalTime)
-    .attr('cx', scale.x(Math.random() * 100))
     .attr('cx', function() {
       return scale.x(Math.random() * 100)
     })
@@ -56,8 +52,52 @@ var moveEnemies = function() {
     });
 };
 
+/******************************************************/
+/******************************************************/
+
+var Player = function () {
+  this.createAndRenderPlayer();
+  // this.setupDragging();
+}
+
+Player.prototype.createAndRenderPlayer = function(){
+  this.playerNode = svgContainer.selectAll('.player')
+    .data([0])
+    .enter()
+    .append('circle')
+    .attr('r', 10)
+    .attr('cx', scale.x(50))
+    .attr('cy', scale.y(50))
+    .classed('player', true)
+    .call(drag);
+    //return player
+};
+
+/******************************************************/
+/***D3 Setup*******************************************/
+
+var drag = d3.behavior.drag().on('drag', function(){
+  player.playerNode.attr('cx', function(){
+    return d3.event.x;
+  })
+  .attr('cy', function(){
+    return d3.event.y;
+  });
+});
+
+
+
+/******************************************************/
+/******************************************************/
+
+var enemies = createEnemies();
+renderEnemies();
+
 moveEnemies();
 setInterval(moveEnemies, gameOptions.intervalTime);
+
+var player = new Player();
+
 
 
 
